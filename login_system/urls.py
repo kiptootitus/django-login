fc7c3dcc980f5f727af9431dcd06e0e748e70631
Titus Kiptoo <68914236+kiptootitus@users.noreply.github.com>
2023-03-06 12:45:14 +0300

    UPDATES

--- login_system/urls.py
+++ login_system/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name='login'),
-    path('welcome/', views.welcome, name='welcome'),
+    path('welcome', views.welcome, name='welcome'),
     # path('register/', views.register, name='register'),
 ]
